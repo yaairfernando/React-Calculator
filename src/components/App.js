4afec284fdcf03e005adbbbaeb2b53d8e4681a2d
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-05-20 17:19:49 -0500

    Added stickler ans eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
-};
+const App = () => (
+  <div>This is my calculator</div>
+);
 
 export default App;
